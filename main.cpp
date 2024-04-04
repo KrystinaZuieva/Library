e31fef5ba5ebce6a0b5603c1f5e7fa94cbfc42ba
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-04-04 20:29:06 +0300

    Test the code.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include "User.h"
+#include "Book.h"
+#include "Library.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Library library1 ("Family leisure club", "Ave. Nezalezhnosti", "08.00-21.00");
+    library1.displayInfo();
+    User user1 ("Krystina", "Zuieva", 17);
+    user1.displayInfo();
+    Book book1 ("Fourth Wing", "Rebecca Yarros", 2023, 528, "Fantasy");
+    cout << "Counter book: " << Book::getCounter() << endl;
+    book1.displayInfo();
+    Book newBook;
+    cin >> newBook;
+    cout << "New book details: \n" << newBook << endl;
+    Book combinedBook = book1 + newBook;
+    combinedBook.printCounter();
+    cout << combinedBook << endl;
+
     return 0;
 }
