a989c4bbfe57942327397267761b6b41fa0e4e85
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-04-04 20:24:06 +0300

    Add attribute and method static, copy constructor, binary operator overload, stream insertion and extraction.

--- Book.h
+++ Book.h
@@ -13,6 +13,7 @@ private:
     int yearOfPublication;
     int numberOfPages;
     string genre;
+    static int counterBook;
 public:
     Book();
 
@@ -26,18 +27,37 @@ public:
 
     Book(string new_title, string new_author, int new_year, int new_numOfPages, string new_genre);
 
-    ~Book() {};
+    ~Book();
 
-    bool single;
+    Book(const Book& other);
 
-    void displayInfo() {
+    void displayInfo() const {
         cout << "Title: " << title << endl
              << "Author: " << author << endl
              << "Year of publication: " << yearOfPublication << endl
              << "Number of pages: " << numberOfPages << endl
-             << "Genre: " << genre << endl;
+             << "Genre: " << genre << endl
+             << "-----------------------------------------" << endl;
     };
 
+    static int getCounter() {
+        return counterBook;
+    }
+
+    Book operator+(const Book &other) const {
+        Book combinedBook(*this);
+        combinedBook.title += " & " + other.title;
+        combinedBook.author += " & " + other.author;
+        return combinedBook;
+    }
+
+    void printCounter() const {
+        cout << "Counter: " << this->counterBook << endl;
+    }
+
+    friend ostream& operator<<(ostream& os, const Book& book);
+
+    friend istream& operator>>(istream& is, Book& book);
 };
 
 #endif
