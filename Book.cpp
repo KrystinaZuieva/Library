c798e01fb0e244567e69659df7a38e71f1c86db7
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-04-04 20:26:43 +0300

    Description attribute static, copy constructor, stream insertion and extraction.

--- Book.cpp
+++ Book.cpp
@@ -1,5 +1,7 @@
 #include "Book.h"
 
+int Book::counterBook = 0;
+
 Book::Book()
         : Book{"None", "None", 0, 0, "None"} {}
 
@@ -17,4 +19,27 @@ Book::Book(std::string new_title, std::string new_author, int new_year, int new_
 
 Book::Book(std::string new_title, std::string new_author, int new_year, int new_numOfPages, std::string new_genre)
         : title(new_title), author(new_author), yearOfPublication(new_year), numberOfPages(new_numOfPages),
-          genre(new_genre) {}
+          genre(new_genre) {
+    counterBook++;
+}
+
+Book::Book(const Book &other):
+        title((other.title)), author((other.author)), yearOfPublication(other.yearOfPublication), numberOfPages(other.numberOfPages), genre((other.genre)) {}
+
+std::ostream& operator<<(std::ostream& os, const Book& book) {
+    os << "Title: " << book.title << endl
+    << "Author: " << book.author << endl;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Book& book) {
+    std::cout << "Enter title: ";
+    std::getline(is, book.title);
+    std::cout << "Enter author: ";
+    std::getline(is, book.author);
+    return is;
+}
+
+Book::~Book() {
+    counterBook--;
+}
