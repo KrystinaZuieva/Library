80bb191f06d9bb843df48d6d5f7d91d91a5bb5f9
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-05-10 08:44:14 +0300

    Add body stream insertion/extraction operator in class Fiction.

--- Fiction.cpp
+++ Fiction.cpp
@@ -11,3 +11,16 @@ Fiction::Fiction(std::string &Title, std::string &Author, int Year) : Book(Title
 Fiction::Fiction(std::string &Title, std::string &Author, int Year, int Pages) : Book(Title, Author, Year, Pages), genre{"None"} {}
 
 Fiction::Fiction(std::string &Title, std::string &Author, int Year, int Pages, double Price) : Book(Title, Author, Year, Pages, Price), genre{"None"} {}
+
+std::ostream& operator<<(std::ostream& os, const Fiction& book) {
+    os << "Genre: " << book.genre << endl
+    << static_cast<const Book&>(book) << endl;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Fiction& book) {
+    std::cout << "Enter genre: ";
+    std::getline(is, book.genre);
+    is >> static_cast<Book&>(book);
+    return is;
+}
