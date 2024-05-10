99817ba05e3694a6d5865db85a79f8c78f74c537
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-05-10 08:43:59 +0300

    Add body stream insertion/extraction operator in class NonFiction.

--- NonFiction.cpp
+++ NonFiction.cpp
@@ -11,3 +11,16 @@ NonFiction::NonFiction(std::string &Title, std::string &Author, int Year) : Book
 NonFiction::NonFiction(std::string &Title, std::string &Author, int Year, int Pages) : Book(Title, Author, Year, Pages), subject{"None"} {}
 
 NonFiction::NonFiction(std::string &Title, std::string &Author, int Year, int Pages, double Price) : Book(Title, Author, Year, Pages, Price), subject{"None"} {}
+
+std::ostream& operator<<(std::ostream& os, const NonFiction& book) {
+    os << "Subject: " << book.subject << endl
+       << static_cast<const Book&>(book) << endl;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, NonFiction& book) {
+    std::cout << "Enter subject: ";
+    std::getline(is, book.subject);
+    is >> static_cast<Book&>(book);
+    return is;
+}
