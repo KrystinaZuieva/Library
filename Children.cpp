8cb602ce4f9f85b87a80377dbbec462934450434
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-05-10 08:43:33 +0300

    Add body stream insertion/extraction operator in class Children.

--- Children.cpp
+++ Children.cpp
@@ -11,3 +11,16 @@ Children::Children(std::string &Title, std::string &Author, int Year) : Book(Tit
 Children::Children(std::string &Title, std::string &Author, int Year, int Pages) : Book(Title, Author, Year, Pages),recommendedAge{0} {}
 
 Children::Children(std::string &Title, std::string &Author, int Year, int Pages, double Price) : Book(Title, Author, Year, Pages, Price), recommendedAge{0} {}
+
+std::ostream& operator<<(std::ostream& os, const Children& book) {
+    os << "Recommended age: " << book.recommendedAge << endl
+       << static_cast<const Book&>(book) << endl;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Children& book) {
+    std::cout << "Enter rec age: ";
+    is >> book.recommendedAge;
+    is >> static_cast<Book&>(book);
+    return is;
+}
